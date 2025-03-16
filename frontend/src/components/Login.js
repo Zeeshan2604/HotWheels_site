2b119f67cdd05c44f06359f7dd7a8d4e1b04ab34
Zeeshan Shaikh <116495634+Zeeshan2604@users.noreply.github.com>
2025-03-16 11:54:14 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await axi.post("http://localhost:3000/api/v1/auth/login", {
+      const response = await axi.post("/api/v1/auth/login", {
         email,
         password
       });
@@ -59,7 +59,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await axi.post("http://localhost:3000/api/v1/auth/google", {
+      const response = await axi.post("/api/v1/auth/google", {
         credential: credentialResponse.credential
       });
 
