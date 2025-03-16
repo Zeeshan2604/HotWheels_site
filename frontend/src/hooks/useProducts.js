39a81bee6150e3025106b68e9ad7a35b22b1899a
Zeeshan Shaikh <116495634+Zeeshan2604@users.noreply.github.com>
2025-03-16 11:41:02 +0530

    Update useProducts.js

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const axi = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.URL,
 });
 
 
